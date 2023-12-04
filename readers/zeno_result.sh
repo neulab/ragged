8deda291596acff01d8c7b83a1e17a5c65256a6d
Jennifer Hsia <jhsia2@andrew.cmu.edu>
2023-12-04 14:03:23 -0500

    add q categorization and example shell scripts:

---
+++ readers/zeno_result.sh
@@ -0,0 +1,5 @@
+# PYTHONPATH=$PYTHONPATH:/home/jhsia2/ragged
+
+python convert_result_to_zeno.py --retriever bm25 --reader flanT5 --dataset nq
+
+# python convert_result_to_zeno.py --retriever bm25 --reader flanT5 --dataset hotpotqa
