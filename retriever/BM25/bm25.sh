9743924b09f6cb14676951e44c284699539d4f45
jenhsia <jhsia2@andrew.cmu.edu>
2024-03-08 23:39:08 -0500

    resolve relative import

--- retriever/BM25/bm25.sh
+++ retriever/BM25/bm25.sh
@@ -1,8 +1,12 @@
-export PYTHONPATH=$PYTHONPATH:$ragged_dir:$pyserini_dir
+#!/bin/bash
+
+SCRIPT_PATH="$(realpath "$0")"
+RAGGED_PATH="${SCRIPT_PATH%/retriever/BM25/bm25.sh}"
+export PYTHONPATH="$PYTHONPATH:$RAGGED_PATH:$KILT_PATH"
 
 conda activate kilt
 
-python ${kilt_dir}scripts/execute_retrieval.py -m bm25 -o ${prediction_dir}/bm25 --test_config KILT/kilt/configs/${dataset}.json
+python ${kilt_dir}/scripts/execute_retrieval.py -m bm25 -o ${prediction_dir}/bm25 --test_config KILT/kilt/configs/${dataset}.json
 
 conda deactivate
 
--- retriever/BM25/get_indices.sh
+++ retriever/BM25/get_indices.sh
@@ -1,13 +1,17 @@
-export PYTHONPATH=$PYTHONPATH:$ragged_dir:$pyserini_dir
+#!/bin/bash
+
+SCRIPT_PATH="$(realpath "$0")"
+RAGGED_PATH="${SCRIPT_PATH%/retriever/BM25/get_indices.sh}"
+export PYTHONPATH="$PYTHONPATH:$RAGGED_PATH:$PYSERINI_PATH"
 
 conda activate kilt
 
 python -m pyserini.index.lucene \
-  --collection JsonCollection \
-  --input ${corpus_dir}/${corpus_name}/${corpus_name}_jsonl \
-  --index ${index_dir}/${corpus_name}jsonl \
-  --generator DefaultLuceneDocumentGenerator \
-  --threads 4 \
-  --storePositions --storeDocvectors --storeRaw --storeContents
+      --collection JsonCollection \
+      --input ${corpus_dir}/${corpus_name}/${corpus_name}_jsonl \
+      --index ${index_dir}/${corpus_name}jsonl \
+      --generator DefaultLuceneDocumentGenerator \
+      --threads 4 \
+      --storePositions --storeDocvectors --storeRaw --storeContents
 
-  conda deactivate
+conda deactivate
