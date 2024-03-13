f7aef2230bf99a42548b907a9db3e5edeab20e3e
afreens <afreens@andrew.cmu.edu>
2024-03-13 15:10:29 -0400

    Removing reader hardcoded paths

--- retriever/ColBERT/colbert.sh
+++ retriever/ColBERT/colbert.sh
@@ -8,15 +8,6 @@ export PYTHONPATH="$PYTHONPATH:$RAGGED_PATH:$COLBERT_PATH"
 export FORCE_CUDA="1"
 rm -rf .cache/torch_extensions/
 
-# export COLBERT_PATH=/home/jhsia2/legacy_RAGGED_ColBERT
-# export prediction_dir=/data/tir/projects/tir6/general/afreens/dbqa/zip_ready/retriever_results/predictions
-# export model_dir=/data/tir/projects/tir6/general/afreens/dbqa/models
-# export corpus_dir=/data/tir/projects/tir6/general/afreens/dbqa/zip_ready
-# export corpus=example_corpus
-# export data_dir=/data/tir/projects/tir6/general/afreens/dbqa/zip_ready
-# export dataset=example_query
-# export index_dir=/data/tir/projects/tir6/general/afreens/dbqa/zip_ready/index_dir
-
 python "$RAGGED_PATH/retriever/ColBERT/get_colbert_predictions.py" --prediction_dir $prediction_dir \
                                                                     --model_dir $model_dir \
                                                                     --corpus_dir $corpus_dir \
