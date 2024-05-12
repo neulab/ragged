2a616c22f5ba42ce8e3ccfe15d624b0a433034dc
jenhsia <jhsia2@andrew.cmu.edu>
2024-05-12 18:08:34 -0400

    remove hardcoded paths

--- retriever/BM25/get_indices.sh
+++ retriever/BM25/get_indices.sh
@@ -2,11 +2,6 @@
 SCRIPT_PATH="$(realpath "$0")"
 RAGGED_PATH="${SCRIPT_PATH%/retriever/BM25/get_indices.sh}"
 export PYTHONPATH="$PYTHONPATH:$RAGGED_PATH"
-# export PYTHONPATH="$PYTHONPATH:$RAGGED_PATH:$PYSERINI_PATH"
-
-export corpus_dir=/data/tir/projects/tir6/general/afreens/dbqa/zip_ready
-export corpus=example_corpus
-export index_dir=/data/tir/projects/tir6/general/afreens/dbqa/zip_ready/index_dir
 
 python -m pyserini.index.lucene \
       --collection JsonCollection \
