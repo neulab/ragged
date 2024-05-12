820a2e30e1cfa8bcd141224f5fafd08d27cbbfd3
jenhsia <jhsia2@andrew.cmu.edu>
2024-05-12 18:06:24 -0400

    remove harcoded filepath

--- retriever/BM25/bm25.sh
+++ retriever/BM25/bm25.sh
@@ -5,11 +5,10 @@ RAGGED_PATH="${SCRIPT_PATH%/retriever/BM25/bm25.sh}"
 export PYTHONPATH="$PYTHONPATH:$RAGGED_PATH"
 
 # export test_config 
-export prediction_dir=/data/tir/projects/tir6/general/afreens/dbqa/zip_ready/retriever_results/predictions
 
 python execute_retrieval.py --output_folder ${prediction_dir}/bm25 \
                             --test_config dataset_config.json\
                             --model_configuration default_bm25.json\
-                            --logdir /data/tir/projects/tir6/general/jhsia2/bm25_logs
+                            --logdir ${prediction_dir}/bm25/logs
 
 # conda deactivate
